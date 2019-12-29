1881ec85e075ae63246517347b8571a0decf378f
Hakob <hapetrosyan@gmail.com>
2019-12-28 20:40:00 -0500

    edit ind

--- libs/indicators.py
+++ libs/indicators.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def simple_moving_average(df, period):
-    df['SMA'] = df.iloc[:,1].rolling(window=period).mean()
-    return df
+def simple_moving_average(df_inp, period):
+    df_out = df_inp
+    df_out['SMA'] = df_out.iloc[:,1].rolling(window=period).mean()
+    return df_out
