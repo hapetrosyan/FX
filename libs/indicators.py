22d0396fd9d1d74ca8f8bf7b180dc4f50d59ff07
Hakob <hapetrosyan@gmail.com>
2019-12-29 09:29:24 -0500

    add ipynb and col name

--- libs/indicators.py
+++ libs/indicators.py
@@ -2,5 +2,6 @@ import pandas as pd
 import numpy as np
 
 
-def add_simple_moving_average(df, period_in_days):
-    df['SMA_'+str(period_in_days)+'_D'] = df.iloc[:,1].rolling(window=period_in_days).mean()
+def add_simple_moving_average(df, period_in_days, col_name=''):
+    # df['SMA_'+str(period_in_days)+'_D'] = df.iloc[:,1].rolling(window=period_in_days).mean()
+    df['SMA_'+str(period_in_days)+'_D'] = df[col_name].rolling(window=period_in_days).mean()
