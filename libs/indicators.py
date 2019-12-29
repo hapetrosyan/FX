bedb1af6729597c33174fec733c75ecd9a8c222d
Hakob <hapetrosyan@gmail.com>
2019-12-29 08:36:43 -0500

    add periods to mov avg func

--- libs/indicators.py
+++ libs/indicators.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 
 
-def simple_moving_average(df_inp, period):
-    df_out = df_inp
-    df_out['SMA'] = df_out.iloc[:,1].rolling(window=period).mean()
-    return df_out
+def add_simple_moving_average(df, period_in_days):
+    df['SMA_'+str(period_in_days)+'_D'] = df.iloc[:,1].rolling(window=period_in_days).mean()
+    return df
